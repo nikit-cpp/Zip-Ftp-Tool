227f099d408557975d2180e8cdf8ac97c2e4c870
nikit.cpp <nikit.cpp@yandex.ru>
2014-06-14 21:01:45 +0300

    .bat fix

--- WinLauncherConsole GUI.bat
+++ WinLauncherConsole GUI.bat
@@ -5,7 +5,7 @@ SET MAVENREPO=%USERPROFILE%\.m2\repository
 
 @echo off
 
-java -Dfile.encoding=Cp866 -classpath ".\target\classes;%MAVENREPO%\commons-net\commons-net\3.3\commons-net-3.3.jar;%MAVENREPO%\commons-configuration\commons-configuration\1.10\commons-configuration-1.10.jar;%MAVENREPO%\commons-lang\commons-lang\2.6\commons-lang-2.6.jar;%MAVENREPO%\commons-logging\commons-logging\1.1.1\commons-logging-1.1.1.jar;%MAVENREPO%\commons-collections\commons-collections\3.2.1\commons-collections-3.2.1.jar;%MAVENREPO%\org\eclipse\core\commands\3.3.0-I20070605-0010\commands-3.3.0-I20070605-0010.jar;%MAVENREPO%\org\eclipse\equinox\common\3.6.200-v20130402-1505\common-3.6.200-v20130402-1505.jar" main.UploadProgress
+java -Dfile.encoding=Cp866 -classpath ".\target\classes;%MAVENREPO%\commons-net\commons-net\3.3\commons-net-3.3.jar;%MAVENREPO%\commons-configuration\commons-configuration\1.10\commons-configuration-1.10.jar;%MAVENREPO%\commons-lang\commons-lang\2.6\commons-lang-2.6.jar;%MAVENREPO%\commons-logging\commons-logging\1.1.1\commons-logging-1.1.1.jar;%MAVENREPO%\commons-collections\commons-collections\3.2.1\commons-collections-3.2.1.jar;%MAVENREPO%\org\eclipse\core\commands\3.3.0-I20070605-0010\commands-3.3.0-I20070605-0010.jar;%MAVENREPO%\org\eclipse\equinox\common\3.6.200-v20130402-1505\common-3.6.200-v20130402-1505.jar" main.GUI
 
 ::if ERRORLEVEL 1 pause
 pause
